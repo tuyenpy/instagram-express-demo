907d9fcf04706f61e9f91b093173069477bb75e6
songthinhvuong87 <songthinhvuong87@gmail.com>
2020-05-18 15:42:11 +0700

    Refactor code

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,7 +1,8 @@
 const User = require('../../model/user.model');
 
 module.exports.signUp = async (req, res) => {
-    let user = await new User({ ...req.body }).save();
+    let { avatar, name, phone, email, password } = req.body;
+    let user = await new User({ avatar, name, phone, email, password }).save();
     res.json(user);
-    
+
 }
