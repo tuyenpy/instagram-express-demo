d4fb1c495de3c40b924c95135ff18414248f67c1
songthinhvuong87 <songthinhvuong87@gmail.com>
2020-05-28 17:36:13 +0700

    Fix validate user

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,19 +1,27 @@
 const User = require('../../model/user.model');
+const { hash, comparePassword } = require('../../config/bcrypt');
+
 
 //create user
 module.exports.signUp = async (req, res) => {
-    let { avatar, name, phone, email, password } = req.body;
-    let user = await new User({ avatar, name, phone, email, password }).save();
-    res.json(user);
+    let { name, phone, email, avatar } = req.body;
+
+    //hash password by bcryptjs
+    let password = hash(req.body.password);
+
+    //create user
+    let user = new User({ name, phone, email, password, avatar });
+
+    user.save()
+        .then()
+        .catch(({ message }) => console.log(message));
+        
+    res.json({user});
 
 }
 
 //login
 module.exports.login = async (req, res) => {
-    let { email, password } = req.body;
-    let user = await User.findOne({
-        email: email
-    });
-
-    res.json(user);
+    let user = res.locals.user;
+    res.json({user});
 }
--- api/route/user.route.js
+++ api/route/user.route.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controller/user.controller');
+const validate = require('../validate/user.validate');
 
 //create user
-router.post('/signup', controller.signUp);
+router.post('/signup', validate.create, controller.signUp);
 
 //login
-router.post('/login', controller.login);
+router.post('/login', validate.login, controller.login);
 
 
 module.exports = router;
